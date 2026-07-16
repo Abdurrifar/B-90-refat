9f8bc13c7a60f72421959abdfa7571c7e6976838
abdur refat <abdurrifat2022@gmail.com>
2026-07-17 05:43:02 +0600

    try to solve

--- part-2-client/src/main.jsx
+++ part-2-client/src/main.jsx
@@ -1,26 +1,54 @@
-import { createRoot } from 'react-dom/client';
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import RootElement from './pages/home/RootElement';
-import { Component } from 'react';
-import Home from './pages/home/Home';
+// import { createRoot } from 'react-dom/client';
+// import './index.css'
+// import { createBrowserRouter, RouterProvider } from 'react-router';
+// import RootElement from './pages/home/RootElement';
+// import { Component } from 'react';
+// import Home from './pages/home/Home';
 
 
-const router = createBrowserRouter([
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <RootElement></RootElement>,
+//     children: [
+//       {
+//         index: true,
+//         Component: Home
+//       }
+//     ]
+//   },
+// ]);
+
+// createRoot(document.getElementById('root')).render(
+
+//   // <RouterProvider router={router} />
+//   <BrowserRouter basename="/B-90-refat"></BrowserRouter>
+// )
+
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootElement from "./pages/home/RootElement";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootElement />,
+      children: [
+        {
+          index: true,
+          Component: Home,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <RootElement></RootElement>,
-    children: [
-      {
-        index: true,
-        Component: Home
-      }
-    ]
-  },
-]);
+    basename: "/B-90-refat",
+  }
+);
 
-createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-
-)
+);
